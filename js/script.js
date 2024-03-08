149efe7e779dfbe17b1e2859a84aee0aa32bba85
adrian.addesso <adrian.addesso@gmail.com>
2024-03-08 16:54:42 -0300

    excercises 1.1 to 1.4

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const firstname = 'Adrian';
+const lastname = 'Addesso';
+let nickname = 'Pampero';
+
+nickname = 'petiso'
+
+let apodo = prompt('Tenes un apodo?');
+alert('El apodo es obligatorio para seguir');
