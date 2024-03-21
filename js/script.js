0487a2b7158d1de9d91b20abbdec1fc4ec9f457b
adrian.addesso <adrian.addesso@gmail.com>
2024-03-21 20:29:05 -0300

    ejercicios

--- js/script.js
+++ js/script.js
@@ -1,51 +1,63 @@
-console.log("Archivo js correctamente vinculado");
-const firstname = 'Adrian';
-const lastname = 'Addesso';
-let apodo = 'sin definir';
-console.log("Tu apodo está ", apodo);
+// Ejercicio 1 ***********************************************************************
 
-let confirmacion = confirm('Deseas cambiar tu apodo?')
-console.log("Cambio de apodo Y/N? ", confirmacion);
-apodo = prompt('Cual es tu apodo?'); // ejemplo de prompt
-alert('Bienvenido a la plataforma '+ apodo); // ejemplo de concatenacion con strings
-console.log("Tu apodo cambió a ", apodo);
-console.log("Hola, ", apodo);
+//1. Crea un algoritmo que a partir de la respuesta del usuario pueda determinar si.
+// A. Esta lloviendo
+// B. Esta soleado
+//En caso de estar lloviendo debe devolver por consola un mensaje que diga "Lleva paraguas"
+//En caso de estar soleado debe devolver por consola un mensaje que diga "No olvides tus shorts"
 
-let calle = prompt('En que calle vivis?');
-let altura = prompt('Cual es la altura de tu casa?');
-let barrio = prompt('En que barrio vivis?');
-let ubicacion = calle +' '+ altura +', '+ barrio;
+//alert("Hola! Veamos como está el clima hoy?");
+//let clima = prompt("Dime, esta lloviendo o está soleado?");
 
-alert('Entonces '+apodo+' vos vivis en '+ ubicacion); // ejemplo de concatenacion con strings
-console.log("Seteaste tu ubicacion en", ubicacion);
+//if((clima=="lloviendo")||(clima=="Lloviendo")){
+//    alert("Lleva paraguas");
+//}else if((clima=="soleado")||(clima=="Soleado")){
+//    alert("No olvides tus shorts")
+//}else{
+//    alert("Por favor repite la respuesta");
+//}
+// Ejercicio 2 **********************************************************************
 
-let Taxi = 300;
-let combustible = 2500;
-let propina = (combustible + Taxi)*0.1;
-let total = Taxi+combustible+propina;
-alert('Un remis hasta '+ubicacion+' te va a costar '+ total +' ARS c/propina')
-console.log("Costo del viaje hasta ",ubicacion," es de ",total);
+// 1. Crea un algoritmo que a partir de la respuesta del usuario pueda determinar si te alcanza para comprar figuritas del mundial.
+// A. Si tienes menos de $20 ó $20 debe devolver por consola un mensaje que diga "Te puedo ofrecer 2 caramelos"
+// B. Si tienes entre de $21 y $49 debe devolver por consola un mensaje que diga "Estas cerca, pero no puedo fiarte, vuelve mañana."
+// C. Si tienes $50 o más debe devolver por consola un mensaje que diga "Aquí tienes tus figuritas del mundial"
 
-//Suma
-let DeptosPiso1 = 10;
-let DeptosPiso2 = 5;
-let cantDeptos = DeptosPiso1 + DeptosPiso2; // 10+5=15
-console.log("Cantidad de departamentos ", cantDeptos);
+// alert("Hola! Visite a comprar Figus? Cada una vale $50...")
+// let budget = parseInt(prompt("Cuanto dinero tenes para gastar?"))
+// let price = 50;
+// if(budget<=0){
+//     alert("Regresa cuando tengas plata");
+// } else if ((budget>=1)&&(budget<=20)){
+//     alert("Te puedo ofrecer 2 caramelos");
+// }else if ((budget>=21)&&(budget<=49)){
+//     alert("Estas cerca, pero no puedo fiarte, vuelve mañana.");
+// } else if(Math.trunc(budget/price)>1){
+//     alert("Aquí tienes: "+(Math.trunc(budget/price))+" paquetes de figuritas del mundial");
+// } else{
+//     alert("Aquí tienes: "+(Math.trunc(budget/price))+" paquete de figuritas del mundial");
+// }
+// Ejercicio 3 **********************************************************************
+// 1. Crea un algoritmo que a partir de la respuesta del usuario pueda determinar Cuántos mundiales vivió una persona.
+// Ten en cuenta que se juega un mundial cada 4 años y que se han jugado 21 copas del mundo.
+let FirstCup = 1930; // Defino cuando comenzaron los torneos
 
-//Multiplicacion
-let departamentos = 40;
-let pisos = 4;
-let cantUF = departamentos * pisos; // 40*4=160
-console.log("Cantidad de unidades funcionales ", cantUF);
+let currentDate = new Date(); //defino la fecha actual
+let currentYear = currentDate.getFullYear();
+     console.log(currentYear);
 
-//Resta
-let Peso = 70;
-let Perdido = 3;
-let PesoFinal = Peso - Perdido; // 70-3=67
-console.log("Peso perdido", PesoFinal);
+let Mundiales = (currentYear - FirstCup);  // defino años pasaron desde el primer mundial
+let Resultado = Math.abs(Mundiales);
+     console.log(Math.trunc(Resultado/4)); // defino cuantos mundiales se jugaron a la fecha
 
-//Division
-let cuenta = 5000;
-let comensales = 5;
-let Vaquita = cuenta / comensales; // 5000/5=1000
-console.log("Precio de la comida por cabeza ", Vaquita);
+let Nacimiento = parseInt(prompt("En qué año naciste?")); // defino el año de nacimiento de la persona
+let Edad = Math.trunc(currentYear-Nacimiento); // defino la edad de la persona
+     console.log(Edad);
+let Vistos = Resultado/Edad
+
+if(Nacimiento<=FirstCup){
+    alert("Si naciste en "+Nacimiento+" entonces tenes "+Edad+" años, por lo cual desde tu nacimiento se han jugado "+Resultado+" desde entonces");
+}else{
+    alert("Si naciste en "+Nacimiento+" entonces tenes "+Edad+" años, por lo cual desde tu nacimiento se han jugado "+Vistos+" desde entonces");
+}
+
