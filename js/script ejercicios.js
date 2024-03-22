2eebb84cf1cd3949d6bb81e3b5de8fe1a5dbfa17
adrian.addesso <adrian.addesso@gmail.com>
2024-03-22 15:04:01 -0300

    Semana 2 excercises complete and stored in Script Ejercicios

--- js/script ejercicios.js
+++ js/script ejercicios.js
@@ -55,3 +55,65 @@
 //     alert("Si naciste en "+Nacimiento+" entonces desde tu nacimiento se han jugado "+Math.trunc(Mundiales)+" Mundiales");
 // }
 
+// Actividad 01:
+/*let cantidad = prompt('INGRESE CANTIDAD DE REPETICIONES');
+let texto = prompt('INGRESE TEXTO A REPETIR');
+for (let index = 0; index < cantidad; index) { //cambiar "index" por "index++
+console.log(texto);
+}*/
+
+//¿Qué tiene que hacer este código? -> Busca repetir un texto una cantidad de veces designada por el usuario
+//¿Por qué no cumple con su función? -> le falta incrementra el valor de index en cada iteracion con index++
+
+// Actividad 02:
+/*let lados = prompt('INGRESE CANTIDAD DE LADOS');
+for (let index = 0; index < lados; index++) {
+if (index > 3) {
+}
+alert("lado");
+}
+// ¿Qué tiene que hacer este código? Pide que se ingrese una cantidad y muestra un mensaje cada vez que la cantidad exceda los 3 lados
+// ¿Por qué no cumple con su función? El alert esta fuera del if
+// ¿Qué propuesta podrías hacer para que tenga sentido su uso? Mover el alert dentro del IF
+*/
+
+// Crea un algoritmo que le pida al usuario que ingrese un número y luego evalúe si ese número es par o impar.
+/*
+let input = parseInt(prompt("Ingrese un numero del 1 al 10"))
+
+if (input<0){
+     alert("Ingresaste un numero negativo, por favor ingresa otro del 1 al 10")
+}else if(input>=11){
+     alert("Ingresaste un numero mayor a 10, por favor ingresa otro del 1 al 10")   
+}else if((input==0)||(input==2)||(input==4)||(input==6)||(input==8)||(input==10)){
+     alert("El número "+input+" es par")
+}else {
+     alert("El número "+input+" es impar")
+}
+*/
+
+// Crea un algoritmo que le pida al usuario un número y luego utiliza while para evaluar cuántos números son pares y cuantos impares,
+//hasta que el usuario ingrese "salir".
+/*
+let input = 0;
+let par = 0;
+let impar = 0;
+
+do{
+    input = prompt("Ingresa un numero del 1 al 10. Cuando desees terminar, escribe 'alir'");
+    console.log("Número Ingresado: "+input);
+    if((input==0)||(input==2)||(input==4)||(input==6)||(input==8)||(input==10)){
+       par = par+1;
+       alert("El numero ingresado ("+input+") es par. Hasta el momento has ingresado "+par+" números pares");
+       console.log(input+" es par");
+    }else if((input==1)||(input==3)||(input==5)||(input==7)||(input==9)){
+     impar = impar+1
+     alert("El numero ingresado ("+input+") es impar. Hasta el momento has ingresado "+impar+" números impares");
+     console.log(input+" es impar");
+    }else {
+     alert("No has ingresado un numbero del 1 al 10");
+     console.log(input+" no es un número válido");
+    }
+}while((input!="Salir")||(input!="salir"));
+alert("Has decidido salir. Ingresaste un total de "+par+" números pares y "+impar+" impares");
+*/
---
+++ js/script w2.js
@@ -0,0 +1,38 @@
+// **FOR()**
+// for(let i=0;i<10;i++){
+//      if(i==5){
+//           alert("Se salteo la iteracion 5 del ciclo");
+//           console.warn("Iteracion n.",i," salteada por la clausula CONTINUE");
+//           continue;
+//      }
+//      alert(i);
+// }
+
+// **WHILE()**
+// let i = 0;
+// while(i<5){
+//      console.log("Iteracion n°"+i);
+//      i++;
+// }
+
+// **DO{}WHILE()**
+//const clave = 10;
+//do{
+//     numero = parseInt(prompt("Ingrese un numero del 1 al 10"));
+//     console.log("user entered: "+ numero);
+//}while(numero != clave);
+
+// **Switch**
+/*let color = prompt("Adiviná cuál es mi color favorito? rojo o verde?");
+
+switch (color) {
+     case "rojo":
+          alert("Nope");
+          break;
+     case "verde":
+          alert("Correcto!");
+          break;
+     default:
+          alert("Ingresa un valor válido");
+          break;
+}*/
